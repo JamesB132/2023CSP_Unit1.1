4763c6fd614bd8bf287e2bbb2e08347ae764d5ff
James <james.bergacs@vtsd.com>
2023-09-19 10:06:44 -0400

    Test File

---
+++ 1-1-2/Casting Practice.py
@@ -0,0 +1,17 @@
+flotsum = 1.999
+jetsum = 65
+um = "uh"
+
+
+print("Initial Values")
+print("flotsum:", flotsum)
+print("jetsum:", jetsum)
+print("um:", um)
+
+jetsum = chr(jetsum)
+flotsum = int(flotsum)
+
+print("Ending Values")
+print("flotsum:", flotsum)
+print("jetsum:", jetsum)
+print("um:", um)
