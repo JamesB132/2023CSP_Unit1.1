419075b4d7b492c99c24f9f0cda0c51a9324c093
James <james.bergacs@vtsd.com>
2023-10-10 09:22:49 -0400

    Test File

--- 1-1-4/Step 18.py
+++ 1-1-4/Step 18.py
@@ -1,28 +1,52 @@
 #   a114_nested_loops_4.py
 import turtle as trtl
-
+poop = 1
 painter = trtl.Turtle()
-painter.penup()
-painter.goto(-200, 0)
-painter.pendown()
+while(1 + poop == 2):
+    painter.penup()
+    painter.goto(-200, 0)
+    painter.pendown()
 
-x = -200
-y = 0
-move_x = 1
-move_y = 1
-while (x < 1):
+    x = -200
+    y = 0
+    move_x = 1
+    move_y = 1
+    while (x < 1):
 
-    while (y < 100):
-        x = x + move_x
-        y = y + move_y
-        painter.goto(x, y)
+        while (y < 100):
+            x = x + move_x
+            y = y + move_y
+            painter.goto(x, y)
+        move_y = -1
+
+        while (y > 0):
+            x = x + move_x
+            y = y + move_y
+            painter.goto(x, y)
+        move_y = 1
+
+    painter.penup()
+    painter.goto(-200, 0)
+    painter.pendown()
+    x = -200
+    y = 0
+    move_x = 1
     move_y = -1
 
-    while (y > 0):
-        x = x + move_x
-        y = y + move_y
-        painter.goto(x, y)
-    move_y = 1
+    while (x < 1):
+
+        while (y > -100):
+            x = x + move_x
+            y = y + move_y
+            painter.goto(x, y)
+        move_y = 1
+
+        while (y < 0):
+            x = x + move_x
+            y = y + move_y
+            painter.goto(x, y)
+        move_y = -1
+
 
 wn = trtl.Screen()
 wn.mainloop()
